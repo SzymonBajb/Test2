cc80d97e26d2fb2dd0a410f2a7aaa9d7080d214d
SzymonBajb <sereksweterek@gmail.com>
2021-09-15 17:26:40 +0200

    Komunikat nr 2a

---
+++ src/database.cpp
@@ -0,0 +1,18 @@
+#include "include/database.h"
+
+
+void Database::write() {
+
+    ofstream db;
+    db.open("db/lists.sl");
+
+    if (db.is_open()) {
+        db << "1\n2\n3\n4\n5\n";
+    }
+    else {
+        cout << "Cannot open file for writing.";
+    }
+
+    db.close();
+
+}
